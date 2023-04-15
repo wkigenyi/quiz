33363a2070df7cbe586032cd4d6a903dcde85ad4
Wilfred Kigenyi <wkigenyi@gmail.com>
2023-04-15 16:35:04 +0300

    Add fonts by link

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,36 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const Fonts = () => (
+    <>
+      <link
+        rel="preconnect"
+        href="https://fonts.googleapis.com"
+      />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@600;700&display=swap"
+      />
+    </>
+  );
+  
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Fonts/>
+      </Head>
       <body>
         <Main />
         <NextScript />
